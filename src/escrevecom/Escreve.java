eb85891fb36adf99e7b47dc6b048f0b75dbdceb8
Bruno <bbweib5@hotmail.com>
2022-01-24 10:25:14 -0300

    Adicionar todos os arquivos

---
+++ src/escrevecom/Escreve.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package escrevecom;
+
+import java.util.concurrent.TimeUnit;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import jssc.SerialPort;
+import jssc.SerialPortException;
+
+/**
+ *
+ * @author Bruno
+ */
+class Escreve {
+
+    SerialPort port;
+
+    public Escreve() {
+        port = new SerialPort("COM1");
+    }
+
+    public void roda() throws InterruptedException {
+        try {
+            port.openPort();
+//            port.setParams(BAUDRATE_4800, DATABITS_8, STOPBITS_1, PARITY_NONE);
+            port.setParams(4800, 8, 1, 0); // alternate technique
+            
+            
+            for (int i = 0; i <= 550; i++) {
+                port.writeString("i0  00000099000");
+                TimeUnit.MILLISECONDS.sleep(300);
+            }
+            System.out.println("end");
+            port.closePort();
+        } catch (SerialPortException ex) {
+            Logger.getLogger(EscreveCom.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+}
---
+++ src/escrevecom/EscreveCom.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package escrevecom;
+
+/**
+ *
+ * @author Bruno
+ */
+public class EscreveCom {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws InterruptedException {
+
+        Escreve e = new Escreve();
+        e.roda();
+
+    }
+
+}
